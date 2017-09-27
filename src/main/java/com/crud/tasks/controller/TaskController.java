7abda0bd2fee3f1adf17f528dfd9af7d971e2173
catherinka32 <kazia32@tlen.pl>
2017-09-27 21:16:58 +0200

    Task 15.3

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -4,26 +4,28 @@ import com.crud.tasks.domain.TaskDto;
 import com.crud.tasks.mapper.TaskMapper;
 import com.crud.tasks.service.DbService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 @RestController
-@RequestMapping("/v1/task")
-public class TaskController {
-    @Autowired
-    private DbService service;
-    @Autowired
-    private TaskMapper taskMapper;
+    @RequestMapping("/v1/task")
+    public class TaskController {
+        @Autowired
+        private DbService service;
+        @Autowired
+        private TaskMapper taskMapper;
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
-    public List<TaskDto> getTasks(){
-        return taskMapper.mapToTaskDtoList(service.getAllTasks());
-    }
-    @RequestMapping(method = RequestMethod.GET, value = "getTask")
-    public TaskDto getTask(){
-        return taskMapper.mapToTaskDto(service.getTaskWithId());
-    }
+        @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+        public List<TaskDto> getTasks(){
+            return taskMapper.mapToTaskDtoList(service.getAllTasks());
+        }
+        @RequestMapping(method = RequestMethod.GET, value = "getTask")
+        public TaskDto getTask(@RequestParam Long id) throws TaskNotFoundException {
+            return taskMapper.mapToTaskDto(service.getTaskWithId(id).orElseThrow(TaskNotFoundException::new));
+        }
 
 //    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
 //    public List<TaskDto> getTasks(){
--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DbService {
@@ -15,7 +16,7 @@ public class DbService {
     public List<Task> getAllTasks(){
       return repository.findAll();
     }
-    public Task getTaskWithId() {
-        return repository.getTaskById();
+    public Optional<Task> getTaskWithId(Long id) {
+        return repository.findById(id);
     }
 }
