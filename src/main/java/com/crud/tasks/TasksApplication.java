d3f8f1ee317e8727600c891ad7acb738629b1696
catherinka32 <kazia32@tlen.pl>
2017-10-16 12:18:18 +0200

    JAR and Posgresql fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import java.time.LocalDate;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer{
+//public class TasksApplication extends SpringBootServletInitializer
+public class TasksApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
-	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
-		return application.sources(TasksApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure (SpringApplicationBuilder application){
+//		return application.sources(TasksApplication.class);
+//	}
 
 }
